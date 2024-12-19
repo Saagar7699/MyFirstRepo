c19b305e50c8a971162b7fd30390e635f250571a
Lakshmiprasanna454 <maddalakshmiprasanna22@gmail.com>
2024-12-19 15:32:26 +0530

    updated palindrome program

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-inp=input('Enter a word')
-if inp==inp[::-1]:
-        print('Pallindrome Sentence')          
+def check_palindrome():
+    inp=input('Enter a word')
+    if inp==inp[::-1]:
+        print('Pallindrome Sentence')   
+        
 print('Hello World')
